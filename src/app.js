7e980e07c3cc73bca94c8a920adbd07f40e4dcf7
liha-engineer <liha.engineer@gmail.com>
2025-07-08 23:05:31 +0900

    refactor: yaml 파일 제거

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import YAML from 'yamljs';
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./utils/swagger/swagger-output.json" with { type : "json" };
+import path from 'path';
 import { config } from './config/config.js';
 import accountsRouter from './routes/accounts.router.js';
 
 const app = express();
-
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile, { explorer: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
