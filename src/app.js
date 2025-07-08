90039bf5e5ba2a84afc52722cf0ba66c55e40204
liha-engineer <liha.engineer@gmail.com>
2025-07-08 10:24:47 +0900

    refactor: accountsRouter 추가

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { config } from './config/config.js';
+import accountsRouter from './routes/accounts.router.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/', [accountsRouter]);
+
 app.listen(config.server.port, config.server.host, () => {
   console.log(
-        `Sever running is on ${config.server.host}:${config.server.port}`,
-      );
+    `Sever running is on ${config.server.host}:${config.server.port}`,
+  );
 });
