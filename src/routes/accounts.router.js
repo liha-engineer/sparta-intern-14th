b3c559cf044fb29d8d0f02248b6ce890aa440972
liha-engineer <liha.engineer@gmail.com>
2025-07-08 22:40:01 +0900

    refactor: 의존성 주입 수정

--- src/routes/accounts.router.js
+++ src/routes/accounts.router.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth.middleware.js';
+import { prisma } from '../utils/prisma/index.js';
 import { UserController } from '../controllers/user.controller.js';
-
+import { UserService } from '../services/user.service.js';
+import { UserRepository } from '../repositories/user.repository.js';
 
 const router = express.Router();
-const userController = new UserController();
+const userRepository = new UserRepository(prisma);
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
+
+
 router.post('/signup', userController.signup);
 
 router.post('/login', userController.login);
