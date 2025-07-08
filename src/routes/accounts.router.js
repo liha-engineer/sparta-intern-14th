8d95fd4d7fb6799e6d7dd441a62fa37bb599f385
liha-engineer <liha.engineer@gmail.com>
2025-07-08 21:15:50 +0900

    refactor: MVC패턴 적용하여 라우터 수정

--- src/routes/accounts.router.js
+++ src/routes/accounts.router.js
@@ -1,105 +1,14 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import textValidation from '../utils/joi/textValidation.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
-import { prisma } from '../utils/prisma/index.js';
-import { findUser } from '../utils/prisma/index.js';
+import { UserController } from '../controllers/user.controller.js';
 
-dotenv.config();
+
 const router = express.Router();
+const userController = new UserController();
+router.post('/signup', userController.signup);
 
-router.post('/signup', async (req, res, next) => {
-  try {
-    const accountTextValidation = await textValidation.account.validateAsync(req.body);
-    const { username, password, nickname } = accountTextValidation;
-    const foundUser = await findUser(username);
+router.post('/login', userController.login);
 
-    if (foundUser)
-      return res.status(409).json({
-        error: {
-          code: 'USER_ALREADY_EXISTS',
-          message: '이미 가입된 사용자입니다.',
-        },
-      });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.accounts.create({
-      data: {
-        username,
-        password: hashedPassword,
-        nickname,
-      },
-    });
-
-    return res.status(201).json({
-      username: `${user.username}`,
-      nickname: `${user.nickname}`,
-    });
-  } catch (e) {
-    console.error('에러 발생: ', e);
-    return res.status(400).json(e.message);
-  }
-});
-
-router.post('/login', async (req, res, next) => {
-  const { username, password } = req.body;
-  const foundUser = await findUser(username);
-  if (!foundUser)
-    return res.status(404).json({
-      error: {
-        code: 'USER_NOT_FOUND',
-        message: '사용자가 존재하지 않습니다.',
-      },
-    });
-
-  const passwordCompare = await bcrypt.compare(password, foundUser.password);
-  if (!passwordCompare)
-    return res.status(401).json({
-      error: {
-        code: 'INVALID_CREDENTIALS',
-        message: '아이디 또는 비밀번호가 올바르지 않습니다.',
-      },
-    });
-
-  const accessToken = jwt.sign({ accountId: username }, process.env.ACCESSTOKEN_KEY, {
-    expiresIn: '3m',
-  });
-
-  return res.status(200).header('authorization', `Bearer ${accessToken}`).json({ token: accessToken });
-});
-
-router.get('/userinfo/:username', authMiddleware, async (req, res, next) => {
-  const { username } = req.params;
-  const requestedUser = req.user;
-  const foundUser = await prisma.accounts.findFirst({
-    where: { username: username },
-    select: {
-      accountUniqueId: true,
-      username: true,
-      nickname: true,
-      createdAt: true,
-    },
-  });
-
-  if (!foundUser)
-    return res.status(404).json({
-      error: {
-        code: 'USER_NOT_FOUND',
-        message: '사용자가 존재하지 않습니다.',
-      },
-    });
-
-  if (requestedUser.username != foundUser.username)
-    return res.status(403).json({
-      error: {
-        code: 'ACCESS_DENIED',
-        message: '접근 권한이 없습니다.',
-      },
-    });
-
-  return res.status(200).json({ account: foundUser });
-});
+router.get('/userinfo/:username', authMiddleware, userController.getUserInfo);
 
 export default router;
